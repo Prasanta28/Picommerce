159a4b0d2a4c49e996cbe93b57730c77f990e71f
Prasanta Sahu <Prasanta@pitangent.com>
2023-10-13 12:58:22 +0530

    add some data in App

--- demo_traders_terminal/src/main/java/com/example/App.java
+++ demo_traders_terminal/src/main/java/com/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println(10+20);
         System.out.println(10+20+50);
+        System.out.println(10+20+500);
     }
 }
